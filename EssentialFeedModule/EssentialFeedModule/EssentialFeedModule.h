da3492e1bd3f61166c9b22539b521df5290f163c
Hiram Castro <hiram2027@gmail.com>
2023-08-08 09:15:17 -0700

    setup essential feed module

---
+++ EssentialFeedModule/EssentialFeedModule/EssentialFeedModule.h
@@ -0,0 +1,18 @@
+//
+//  EssentialFeedModule.h
+//  EssentialFeedModule
+//
+//  Created by Hiram Castro on 08/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeedModule.
+FOUNDATION_EXPORT double EssentialFeedModuleVersionNumber;
+
+//! Project version string for EssentialFeedModule.
+FOUNDATION_EXPORT const unsigned char EssentialFeedModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeedModule/PublicHeader.h>
+
+
