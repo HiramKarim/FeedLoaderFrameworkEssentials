2d32bc9dac2143f8121c739e9f89969966af1f66
Hiram Castro <hiramcastrodeveloper@gmail.com>
2023-08-15 15:11:26 -0700

    implementing DTO to hide higher model implementation and hide sensitive server data

--- EssentialFeedModule/EssentialFeedModule/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeedModule/EssentialFeedModule/FeedAPI/RemoteFeedLoader.swift
@@ -45,7 +45,7 @@ public final class RemoteFeedLoader {
             case let .success(data, response):
                 if response.statusCode == 200,
                     let root = try? JSONDecoder().decode(Root.self, from: data) {
-                    completion(.success(root.items))
+                    completion(.success(root.items.map { $0.item }))
                 } else {
                     completion(.failure(.invalidData))
                 }
@@ -58,5 +58,19 @@ public final class RemoteFeedLoader {
 }
 
 private struct Root: Decodable {
-    let items: [FeedItem]
+    let items: [Item]
+}
+
+private struct Item: Decodable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL?
+    
+    var item: FeedItem {
+        return FeedItem(id: id,
+                        description: description,
+                        location: location,
+                        imageURL: imageURL)
+    }
 }
