18d4ca1c2829700bf19babc885b5374be5a2dd3d
Hiram Castro <hiramcastrodeveloper@gmail.com>
2023-08-16 13:42:25 -0700

    simplify error type

--- EssentialFeedModule/EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeedModule/EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -60,7 +60,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         let clientError = NSError(domain: "Test", code: 0)
         
         ///When
-        expect(sut, toCompleteWithResult: .failure(RemoteFeedLoader.Error.invalidData)) {
+        expect(sut, toCompleteWithResult: failure(RemoteFeedLoader.Error.invalidData)) {
             ///Then
             client.complete(with: clientError)
         }
@@ -71,7 +71,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         let samples = [199, 201, 300, 400, 500]
         
         samples.enumerated().forEach { index, statusCode in
-            expect(sut, toCompleteWithResult: .failure(RemoteFeedLoader.Error.invalidData)) {
+            expect(sut, toCompleteWithResult: failure(RemoteFeedLoader.Error.invalidData)) {
                 let json = makeItemsJSON([])
                 client.complete(with: statusCode, data: json, at: index)
             }
@@ -81,7 +81,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOn200HTTPResponsewithInvalidJSON() {
         let (sut, client) = makeSUTandClientHTTP()
         
-        expect(sut, toCompleteWithResult: .failure(RemoteFeedLoader.Error.invalidData)) {
+        expect(sut, toCompleteWithResult: failure(RemoteFeedLoader.Error.invalidData)) {
             let invalidJSON = Data("invalid json".utf8)
             client.complete(with: 200, data: invalidJSON)
         }
@@ -145,6 +145,10 @@ final class RemoteFeedLoaderTests: XCTestCase {
         trackForMemoryLeaks(client)
         
         return (sut, client)
+    }
+    
+    private func failure(_ error: RemoteFeedLoader.Error) -> RemoteFeedLoader.Result {
+        return .failure(error)
     }
     
     private func trackForMemoryLeaks(_ instance: AnyObject,
