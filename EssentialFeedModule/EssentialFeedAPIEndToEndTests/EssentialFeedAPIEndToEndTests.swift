3adde5cc38c98637ec9e20736215025271dfc31d
Hiram Castro <hiram2027@gmail.com>
2023-09-06 13:26:14 -0700

    refactoring helper method

--- EssentialFeedModule/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedModule/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -11,7 +11,18 @@ import EssentialFeedModule
 final class EssentialFeedAPIEndToEndTests: XCTestCase {
     
     func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
-        
+        switch getFeedResult() {
+        case let .success(items)?:
+            XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed.")
+        case let .failure(error)?:
+            XCTFail("Expected successful feed result, got \(error) instead")
+        default:
+            XCTFail("Expected successful feed result, got no results instead")
+        }
+    }
+    
+    //MARK: - Helper functions
+    func getFeedResult() -> LoadFeedResult? {
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient()
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
@@ -27,15 +38,7 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
         
         wait(for: [exp], timeout: 5.0)
         
-        switch receivedResult {
-        case let .success(items)?:
-            XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed.")
-        case let .failure(error)?:
-            XCTFail("Expected successful feed result, got \(error) instead")
-        default:
-            XCTFail("Expected successful feed result, got no results instead")
-        }
-        
+        return receivedResult
     }
     
 }
