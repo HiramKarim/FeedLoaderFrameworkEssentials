01420f8e4f3b7ecb788a0d21b927f2519c694d9c
Hiram Castro <hiram2027@gmail.com>
2023-09-06 20:14:28 -0700

    Improving data races
    Finish URL loading when observing requests to make sure all URL requests are finished before the test method returns. This way, we prevent data races with threads living longer that the test method that initiated them.

--- EssentialFeedModule/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedModule/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -224,11 +224,15 @@ private class URLProtocolStub: URLProtocol {
     }
     
     override class func canInit(with request: URLRequest) -> Bool {
-        requestObserver?(request)
         return true
     }
     
     override func startLoading() {
+        
+        if let requestObserver = URLProtocolStub.requestObserver {
+            client?.urlProtocolDidFinishLoading(self)
+            return requestObserver(request)
+        }
         
         if let data = URLProtocolStub.stub?.data {
             client?.urlProtocol(self, didLoad: data)
