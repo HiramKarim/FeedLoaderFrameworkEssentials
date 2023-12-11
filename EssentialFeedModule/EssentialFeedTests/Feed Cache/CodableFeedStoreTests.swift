e74b8d1dbb8a6d98b36a00d5ae27a6c89951d629
Hiram Castro <hiramcastrodeveloper@gmail.com>
2023-12-11 15:36:23 -0800

    renaming test method name for convemtion

--- EssentialFeedModule/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedModule/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -92,7 +92,7 @@ final class CodableFeedStoreTests: XCTestCase {
         expect(sut, toRetrieveTwice: .empty)
     }
     
-    func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
         ///GIVEN
         let sut = makeSUT()
         let feed = uniqueImageFeed().local
