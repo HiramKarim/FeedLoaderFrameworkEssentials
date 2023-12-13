1db38a41eeb2be9bdb276bdbfdc1eb28306a5b7f
Hiram Castro <hiram2027@gmail.com>
2023-12-13 14:54:16 -0800

    /// The completion handler can be invoked in any thread.     /// Clients are responsible to dispatch to appropriate threads, if needed.     func insert(_ feed: [LocalFeedImage], _ timestamp: Date, completion: @escaping InsertionCompletion)

--- EssentialFeedModule/EssentialFeedModule/Feed Cache/FeedStore.swift
+++ EssentialFeedModule/EssentialFeedModule/Feed Cache/FeedStore.swift
@@ -21,6 +21,12 @@ public protocol FeedStore {
     /// The completion handler can be invoked in any thread.
     /// Clients are responsible to dispatch to appropriate threads, if needed.
     func insert(_ feed: [LocalFeedImage], _ timestamp: Date, completion: @escaping InsertionCompletion)
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
+    func insert(_ feed: [LocalFeedImage], _ timestamp: Date, completion: @escaping InsertionCompletion)
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
+    func insert(_ feed: [LocalFeedImage], _ timestamp: Date, completion: @escaping InsertionCompletion)
     func retrieve(completion: @escaping RetrievalCompletion)
 }
